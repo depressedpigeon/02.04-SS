c94f6a7561ac044424dcc794fb12be594b21196e
olakupczyk <10407738@ucn.dk>
2024-04-04 11:14:35 +0200

    created pacman

---
+++ 02.04-SS/Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Animator animator;
+
+    [Header("-- Private references --")]
+    [SerializeField] private int coinsCollected = 0;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetInteger("Walk", 4);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetInteger("Walk", 3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetInteger("Walk", 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetInteger("Walk", 2);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 newPosition = transform.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            newPosition.x -= moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPosition.x += moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            newPosition.y += moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            newPosition.y -= moveSpeed * Time.deltaTime;
+        }
+        transform.position = newPosition;
+    }
+
+    // OnTriggerEnter2D is called once, when two GameObjects with Collider2Ds hit each other
+    // - One GameObject must have a Rigidbody2D as well
+    // - One of the Collider2Ds must be a Trigger
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Coin"))
+        {
+            coinsCollected += 1;
+            Destroy(coll.gameObject);
+        }
+    }
+
+}
